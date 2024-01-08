0ed9124d5588940f9843917a1e80b5ab5b78fc5a
sityu11 <911sera@gmail.com>
2024-01-08 18:01:46 +0100

    update-240108-1800

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // script.js
 
 const products = [
-    { name: "Rock munkavédelmi bakancs", price: 1000, description: "Rock munkavédelmi bakancs, acélkaplis, taplemezes. Minősítés: CE, EN, ISO200345:2011, S1P SRC", image: "image/rock.bakancs.jpg" }
+    { name: "Rock munkavédelmi bakancs", price: 1000, quantity: "10 db", description: "Rock munkavédelmi bakancs, acélkaplis, taplemezes. Minősítés: CE, EN, ISO200345:2011, S1P SRC", image: "image/rock.bakancs.jpg" },
+    { name: "Kartondoboz E0 (78x78x82)", price: 45, quantity: "150 db", description: "Kartondoboz E0 Mérete: 78x82x82 mm", image: "image/kartondoboz-e0.jpg" },
+    { name: "Kartondoboz F0 (140x190x45)", price: 100, quantity: "120 db", description: "Kartondoboz F0 Mérete: 140x190x45 mm", image: "image/kartondoboz-f0.jpg" },
+    { name: "Kartondoboz F1 (140x190x60)", price: 120, quantity: "50 db", description: "Kartondoboy F1 Mérete: 140x190x60 mm", image: "image/kartondoboz-f0.jpg" },
   ];
   
   function displayProducts(productsToShow) {
@@ -19,16 +22,22 @@ const products = [
       const productName = document.createElement('h2');
       productName.textContent = product.name;
   
-      const productPrice = document.createElement('p');
+      const productPrice = document.createElement('h3');
       productPrice.textContent = `Ár: ${product.price} Ft`;
   
       const productDesc = document.createElement('p');
+      productDesc.classList.add('product-desc');
       productDesc.textContent = product.description;
+
+      const productQuantity = document.createElement('p');
+      productQuantity.classList.add('product-quantity');
+      productQuantity.textContent = product.quantity;
   
       productCard.appendChild(productImg);
       productCard.appendChild(productName);
       productCard.appendChild(productPrice);
       productCard.appendChild(productDesc);
+      productCard.appendChild(productQuantity);
   
       productGrid.appendChild(productCard);
     });
